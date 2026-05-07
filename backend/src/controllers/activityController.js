2d53f47f80ebe46fea25a38c7a4ff9d1b7d3882c
Abhishek Jaiswar <devabhishekjaiswar@gmail.com>
2026-05-08 00:54:26 +0530

    feat: complete the replay check api

--- backend/src/controllers/activityController.js
+++ backend/src/controllers/activityController.js
@@ -46,3 +46,44 @@ export async function logActivity(req, res) {
     });
   }
 }
+
+export async function replayCheck(req, res) {
+  try {
+    const { action } = req.body;
+
+    const clientTime = new Date(req.body.clientTime);
+    const diff = Math.abs(Date.now() - clientTime.getTime()) / 1000;
+
+    if (diff > 30) {
+      return res.status(400).json({
+        allowed: false,
+        message: "Client time difference too large",
+      });
+    }
+
+    const threeSecondsAgo = new Date(Date.now() - 3000);
+
+    const existingAction = await ActivityLog.findOne({
+      userId: req.user.id,
+      action,
+      createdAt: { $gte: threeSecondsAgo },
+    });
+
+    if (existingAction) {
+      return res.status(400).json({
+        allowed: false,
+        message: "Duplicate action detected",
+      });
+    }
+
+    return res.status(200).json({
+      allowed: true,
+      serverTime: new Date().toISOString(),
+    });
+  } catch (error) {
+    console.error("Error in replayCheck controller", error);
+    return res.status(500).json({
+      message: "Internal Server Error hai",
+    });
+  }
+}
--- backend/src/routes/activityRoutes.js
+++ backend/src/routes/activityRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { logActivity } from "../controllers/activityController.js";
+import { logActivity, replayCheck } from "../controllers/activityController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", authMiddleware, logActivity);
+router.post("/replay-check", authMiddleware, replayCheck);
 
 export default router;
